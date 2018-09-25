32d67f299e35103e762ceaf647534688f7510585
Jason Stollman <jason.stollman@gmail.com>
2018-09-25 10:57:49 -0500

    Initial Commit

---
+++ src/providers/apod/apod.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Apod } from '../../apod';
+
+
+/*
+  Generated class for the ApodProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApodProvider {
+
+  private domain:string='https://api.nasa.gov/planetary/';
+  private args:string='apod?api_key=';
+  private nasaKey:string = 'N9I5L8HjYqChYp4VtLvJ3OInAnNM7QNtp5F1ZbRk';
+  private url:string;
+
+  constructor(private http:HttpClient){}
+
+  private setUrl(date=''){
+    if(date){
+      date=`&date=${date}`;
+    }
+
+    this.url = `${this.domain}${this.args}${this.nasaKey}${date}`;
+  }
+
+  public getApod(date=''): Observable<Apod>{
+
+    this.setUrl(date);
+
+    return this.http.get<Apod>(this.url);
+  }
+}
